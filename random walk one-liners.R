fd585c70f85c184a52fa75086418097a81f530d3
Doug Miller <dmiller@udel.edu>
2017-11-29 13:24:14 -0500

    double the fun ...

--- random walk one-liners.R
+++ random walk one-liners.R
@@ -6,3 +6,14 @@ ts.plot(replicate(100,cumsum(rnorm(100))))
 ts.plot(replicate(100,cumsum(runif(100))))
 ts.plot(replicate(100,cumsum(runif(100)-0.5)))
 ts.plot(replicate(100,cumsum(rnorm(100))))
+
+# double the fun:
+  
+ts.plot(replicate(10,cumsum(rnorm(5))))
+ts.plot(replicate(10,cumsum(rnorm(50))))
+ts.plot(replicate(10,cumsum(rnorm(10))))
+ts.plot(replicate(10,cumsum(rnorm(100))))
+ts.plot(replicate(100,cumsum(rnorm(100))))
+ts.plot(replicate(100,cumsum(runif(100))))
+ts.plot(replicate(100,cumsum(runif(100)-0.5)))
+ts.plot(replicate(100,cumsum(rnorm(100))))
