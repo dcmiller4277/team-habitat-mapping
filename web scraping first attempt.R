2a2e08787c38cf08dad72ba4f7933c0da15c4fc6
Doug Miller <dmiller@udel.edu>
2017-11-29 14:11:17 -0500

    maybe?

---
+++ web scraping first attempt.R
@@ -0,0 +1,27 @@
+# web scraping and data munging, 4 oct 17
+
+rm(list=ls())
+
+library(rvest)
+require(dplyr)
+
+aq <- read_html("https://en.wikipedia.org/wiki/Anscombe%27s_quartet")
+
+aqlist <- aq %>% html_table(header=TRUE)
+
+aqlist[[2]]
+
+aqtab <-aqlist[[2]]
+aqtab
+class(aqtab)
+names(aqtab)
+class(aqtab$I)
+aqtab <- aqtab[-1,]
+aqtab
+
+apply(aqtab,2,as.numeric)
+
+aqtab <- apply(aqtab,2,as.numeric)
+
+plot(aqtab)
+
