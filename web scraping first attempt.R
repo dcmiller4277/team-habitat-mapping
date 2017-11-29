d35532d10e87f305983e115d5b2ea4894372a3f4
Doug Miller <dmiller@udel.edu>
2017-11-29 14:57:22 -0500

    removed lines

--- web scraping first attempt.R
+++ web scraping first attempt.R
@@ -1,17 +1,11 @@
 # web scraping and data munging, 4 oct 17
 
 rm(list=ls())
-
 library(rvest)
 require(dplyr) 
-
-
 aq <- read_html("https://en.wikipedia.org/wiki/Anscombe%27s_quartet")
-
 aqlist <- aq %>% html_table(header=TRUE)
-
 aqlist[[2]]
-
 aqtab <-aqlist[[2]]
 aqtab
 class(aqtab)
